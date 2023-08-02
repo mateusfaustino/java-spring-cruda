b0d11c0d1335c7f06cfe6c995848be7a524aeb05
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-08-02 09:18:10 -0300

    init spring project

---
+++ cruda/src/main/java/com/mateusfaustino/cruda/CrudaApplication.java
@@ -0,0 +1,13 @@
+package com.mateusfaustino.cruda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudaApplication.class, args);
+	}
+
+}
---
+++ cruda/src/test/java/com/mateusfaustino/cruda/CrudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusfaustino.cruda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
